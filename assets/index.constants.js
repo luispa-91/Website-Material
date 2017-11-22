488a321f819b0f6a7ef461534cfdd51e93624979
Luis <luispa91@gmail.com>
2017-11-21 23:51:36 -0600

    Initial continuous deployment

---
+++ assets/index.constants.js
@@ -0,0 +1,20 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('angular')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment)
+    .constant('APP_INFO',{
+        'ID': 74,
+        'name': 'Tiendas Blume',
+        'directory': 'demoshop',
+        'website_url': 'https://website2.madebyblume.com/',
+        'contact': {
+        	'email': 'info@madebyblume.com',
+        	'address': 'San Jose, Costa Rica.',
+        	'phone': '+506 7012 5006'
+        }
+      });
+})();
