11d108bba9acfcff247da9b88514c679ebdfd2a1
Luis <luispa91@gmail.com>
2017-12-13 11:30:37 -0600

    Added parameters for chatBubble and cleaned urls from hashtag

---
+++ page_header.php
@@ -0,0 +1,6 @@
+<?php
+$tpTitle="Helping you to improve your web site";
+$pgHeading="Site-Report.com - Helping you to improve your web site";
+$pgDesc="Helping you to improve your web site";
+$pgKeywords="site-report";
+?>
