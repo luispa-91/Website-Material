b7ebfa66cb8ca3af6e5ea26b6414c47a3d318a44
Luis <luispa91@gmail.com>
2018-03-12 00:05:38 -0600

    New style for about us.

--- assets/constants.js
+++ assets/constants.js
@@ -8,9 +8,9 @@
     .constant('moment', moment)
     .constant('APP_INFO',{
         'ID': 74,
-        'name': 'Blume Demo',
+        'name': 'Demo Tienda',
         'directory': 'demoshop',
-        'website_url': 'https://website2.madebyblume.com/',
+        'website_url': 'https://demo.madebyblume.com/',
         'international_shipping': true,
         'gateway': {
             'url': 'http://www.fttserver.com:4217',
@@ -18,14 +18,16 @@
             'applicationPassword': 'Shops082017%'
         },
         'bac': {
-            'key_id': '',
-            'processor_id': '',
-            'applicationPassword': ''
+            'key_id': '7185696',
+            'processor_id': 'freaks2',
+            'applicationPassword': 'CAwDP8vg6wbxP42FS775r6Q8RfB2j2Ep'
         },
         'chat': {
             'order': 'facebook,whatsapp',
             'facebook': '754771351335371',
-            'whatsapp': '+50670125006'
+            'whatsapp': '+50670125006',
+            'shortMessage': 'Envíenos un mensaje',
+            'longMessage': 'Necesita ayuda? Escríbanos y estaremos en contacto pronto'
         },
         'instagram': {
             'account': 'blumeapps',
